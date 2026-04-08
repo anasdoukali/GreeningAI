43546e429d04a7e154bab113f41ddce7fd254ca3
Anas Doukali <doukalianas2@gmail.com>
2026-04-08 22:39:26 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,1 @@
-# In this file, we list all the global variables (like folder names, etc.) 
-# so that we can import them in any file
 
-WORKSPACE = "aymane-lahgazi-wcpmp"
-PROJECT = "test_project-nbeu2"
-DATA_ROBOFLOW = "data_roboflow"
-WEIGHTS = "weights"
-IMPORTED = "imported_images"
-PREDICTIONS = "predictions"
-DATASET_UPLOAD = "dataset_to_upload"
-DATA_READY = "data_ready"
